a6ddac6f90618fa69b2dbe3e13de14fbc11ff404
Roel Hogervorst <hogervorst.rm@gmail.com>
2018-04-03 08:06:38 +0200

    basic generation of markov chains works

--- markov_chains.R
+++ markov_chains.R
@@ -1,16 +1,14 @@
 ###
 library(markovifyR)
-lessons <-
-    linneman_lessons %>%
-    pull(textLesson)
-
-lessons %>% str()
-
+library(dplyr)
+df_sn <- readr::read_rds("df_sn.RDS")
+steveonly <- df_sn %>% tidyr::unnest(text) %>%
+    filter(speaker == "STEVE")
 #Step 2 -- Build the Model
 
 markov_model <-
     generate_markovify_model(
-        input_text = lessons,
+        input_text = steveonly$text, # maybe filter on length >5?
         markov_state_size = 2L,
         max_overlap_total = 25,
         max_overlap_ratio = .85
@@ -18,12 +16,26 @@ markov_model <-
 
 #Step 3 -- Generate the Text
 
+texts_gen <- markovify_text(
+    markov_model = markov_model,
+    maximum_sentence_length = NULL,
+    output_column_name = 'steve',
+    count = 300,
+    tries = 100,
+    only_distinct = TRUE,
+    return_message = FALSE
+)
+readr::write_rds(texts_gen, "texts_gen.rds")
+
 markovify_text(
     markov_model = markov_model,
     maximum_sentence_length = NULL,
-    output_column_name = 'textLinnemanBot',
+    start_words = c("The", "You", "Life"),
+    output_column_name = 'steve',
     count = 25,
     tries = 100,
     only_distinct = TRUE,
     return_message = TRUE
 )
+
+generate_start_words(markov_model = markov_model)
